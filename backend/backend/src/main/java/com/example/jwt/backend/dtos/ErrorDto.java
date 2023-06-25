f8a02b466beb23e78276af2b7ddeaf2500bd5030
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-06-25 09:29:38 +0300

    Add frontend project

---
+++ backend/backend/src/main/java/com/example/jwt/backend/dtos/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.example.jwt.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class ErrorDto {
+
+    private String message;
+}
